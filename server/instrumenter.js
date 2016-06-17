868c7591997ed8244a152de68d872edc0aabe1ec
serut <l.mieulet@gmail.com>
2016-06-17 18:49:35 +0200

    Add more logs for CI - fix eslint

--- server/instrumenter.js
+++ server/instrumenter.js
@@ -46,7 +46,7 @@ if (IS_COVERAGE_ACTIVE) {
             }
         }
         return allowed;
-    }
+    };
 
     shallInstrumentClientScript = function (fileurl) {
         if (fileurl.indexOf('.js') > -1) {
